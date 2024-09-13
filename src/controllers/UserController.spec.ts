09074eeb2c489400c3f50daa14e75926c8bd622f
PCJRibeiro <pcjr2003@gmail.com>
2024-09-13 15:22:03 -0300

    finalizado

---
+++ src/controllers/UserController.spec.ts
@@ -0,0 +1,57 @@
+import { UserController } from "./UserController";
+import { UserService } from '../services/UserService'
+import { Request } from 'express'
+import { makeMockResponse } from "../__mocks__/mockResponse.mock";
+import { makeMockRequest } from "../__mocks__/mockRequest.mock";
+
+describe('UserController', () => {
+    const mockUserService: Partial<UserService> = {
+        createUser: jest.fn(),
+        getAllUsers: jest.fn()
+    }
+    
+    const userController = new UserController(mockUserService as UserService);
+
+    it('Deve adicionar um novo usuário', () => {
+        const mockRequest = {
+            body: {
+                name: 'Nath',
+                email: 'nath@test.com'
+            }
+        } as Request
+        const mockResponse = makeMockResponse()
+        userController.createUser(mockRequest, mockResponse)
+        expect(mockResponse.state.status).toBe(201)
+        expect(mockResponse.state.json).toMatchObject({ message: 'Usuário criado' })
+    })
+    it('Deve exibir um erro por não receber o nome do usuário', ()=>{
+        const mockRequest = {
+            body: {
+                name: '',
+                email: 'nath@test.com'
+            }
+        } as Request
+        const mockResponse = makeMockResponse()
+        userController.createUser(mockRequest, mockResponse)
+        expect(mockResponse.state.status).toBe(400)
+        expect(mockResponse.state.json).toMatchObject({ message: 'Bad request! Name obrigatório' })
+    })
+    it('Deve retorna todos os usuários cadastrados', ()=>{
+        const mockRequest = {} as Request
+        const mockResponse = makeMockResponse()
+        userController.getAllUsers(mockRequest,mockResponse)
+        expect(mockResponse.state.status).toBe(200)
+    })
+    it('Deve exibir um erro por não receber o email do usuário', ()=>{
+        const mockRequest = {
+            body: {
+                name: 'nath',
+                email: ''
+            }
+        } as Request
+        const mockResponse = makeMockResponse()
+        userController.createUser(mockRequest, mockResponse)
+        expect(mockResponse.state.status).toBe(400)
+        expect(mockResponse.state.json).toMatchObject({ message: 'Bad request! email obrigatório' })
+    })
+})
---
+++ src/services/UserService.ts
@@ -0,0 +1,45 @@
+export interface User {
+    name: string
+    email: string
+}
+
+const db = [
+    {
+        name: "Joana",
+        email: "joana@dio.com",
+    }
+]
+
+export class UserService {
+    db: User[]
+
+    constructor(
+        database = db
+    ){
+        this.db = database
+    }
+
+    createUser = (name: string, email: string) => {
+        const user = {
+            name,
+            email
+        }
+
+        this.db.push(user)
+        console.log('DB atualizado', this.db)
+    }
+
+    getAllUsers = () => {
+        return this.db
+    }
+
+    deleteUser = (name: string, email: string) =>{
+        const user ={
+            name,
+            email
+        }
+        const index = this.db.indexOf(user)
+        this.db.splice(index, 1)
+    }
+}
+
